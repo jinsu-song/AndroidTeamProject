778cc6814cc4bf444fa95380a64410f5d3515d59
SamSung <jinhy921207@naver.com>
2021-03-24 13:44:11 +0900

    HayoungActivity commit

---
+++ app/src/main/java/com/sample/androidteamproject/HayoungActivity.java
@@ -0,0 +1,14 @@
+package com.sample.androidteamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HayoungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hayoung);
+    }
+}
